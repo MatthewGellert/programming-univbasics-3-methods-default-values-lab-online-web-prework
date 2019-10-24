8b8bf20c705b2e0987508d648c9b56c96006b5b1
Matthew Gellert <kifireguy@gmail.com>
2019-10-24 22:47:50 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
 def meal_choice
+  puts #{meal_choice} "what a delicious meal"
   puts "A plate of #{veg1} and #{veg 2}."
   puts "additional option of plate #{protein}, with #{veg 1} and #{veg 2}."
 end
