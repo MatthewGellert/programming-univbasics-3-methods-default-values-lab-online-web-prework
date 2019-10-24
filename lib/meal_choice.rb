35b7985b53a668b7a134102ea09cce297061a631
Matthew Gellert <kifireguy@gmail.com>
2019-10-24 23:01:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,6 @@
-def what_a_nutrious_meal
-  puts "a plate of #{protein} with #{veg1} and #{veg2}"
+print Wedding_meal
+def nutrious_meal
+  puts "what_a_nutritious_meal"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
 end
+
